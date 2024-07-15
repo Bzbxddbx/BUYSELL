5834d72dbdd0b046e4f8e0652faa9a1e98d23cde
Sergey <dddf4014@gmail.com>
2024-07-15 23:01:46 +1000

    adding a user-info page

---
+++ buysell/src/main/java/com/example/buysell/configurations/SecurityConfig.java
@@ -0,0 +1,62 @@
+package com.example.buysell.configurations;
+
+import com.example.buysell.services.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@Configuration
+public class SecurityConfig {
+    private final CustomUserDetailsService userDetailsService;
+    private AuthenticationManagerBuilder auth;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/", "/product/**", "/images/**", "/registration")
+                        .permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout(LogoutConfigurer::permitAll
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(8);
+    }
+
+    @Autowired
+    public void setAuth(AuthenticationManagerBuilder auth) {
+        this.auth = auth;
+    }
+
+    @Bean
+    @Primary
+    public AuthenticationManagerBuilder configureGlobal() throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+        return auth;
+    }
+}
+
+
+
+
+
+
---
+++ buysell/src/main/java/com/example/buysell/services/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.example.buysell.services;
+
+import com.example.buysell.repositories.UserRepository;
+import com.example.buysell.models.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+}
+
